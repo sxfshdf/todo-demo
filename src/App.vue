d31d971622b65219f0e1864d377404d08cafcd11
sixiaofeng <sxf754872493@gmail.com>
2019-03-15 21:07:40 +0800

    完成基本功能

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,117 @@
 <template>
   <div id="app">
-    <div class="wrapper">
-      <header>Todos</header>
-      <input type="text" class="todo" placeholder="输入清单">
-      <section>
-        <ul class="list">
-          <li class="item">
-            <el-checkbox v-model="completed" @change="xxx"></el-checkbox>
-            <label>吃饭</label>
-            <input type="text" class="todo-item">
-          </li>
-          <li class="item">
-            <el-checkbox v-model="completed" @change="xxx"></el-checkbox>
-            <label>吃饭</label>
-            <input type="text" class="todo-item">
-          </li>
-          <li class="item">
-            <el-checkbox v-model="completed" @change="xxx"></el-checkbox>
-            <label>吃饭</label>
-            <input type="text" class="todo-item">
-          </li>
-        </ul>
-      </section>
-    </div>
+    <header>
+      <h1>TodoList</h1>
+      <input type="text" class="todo-input" placeholder="你想干点啥" @keydown.enter="addTodo" v-model="todoContent">
+    </header>
+    <section class="main">
+      <ul class="todo-list">
+        <li class="todo-item"
+            v-for="(item, index) in todoList" :key="index"
+            :class="{completed: item.complete}"
+        >
+          <span class="item-content">{{item.content}}</span>
+          <input type="text" class="edit-content" :ref="item.id"
+                 @blur="comfirmEdit(item)"
+                 @keydown.enter="comfirmEdit(item)"
+                 @keydown.esc="cancelEdit($event,item)"
+                 v-model="item.content"
+          >
+          <div class="actions">
+            <i class="check"
+               @click="completeTodo(item)"
+               :class="[item.complete? 'el-icon-refresh' : 'el-icon-circle-check-outline']"
+            ></i>
+            <i class="el-icon-edit edit" @click="editTodo($event,item)"></i>
+            <i class="el-icon-delete delete" @click="removeTodo(item)"></i>
+          </div>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
 <script>
 
+const STORAGE_KEY = 'my-todo'
+
+const todoStorage = {
+  save(todoList){
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todoList))
+  },
+  get(){
+    return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+  }
+}
 
 export default {
   name: 'app',
   data(){
     return {
-      completed: false
+      completed: false,
+      todoList: [],
+      todoContent: '',
+      editingTodo: ''
     }
   },
   components: {
 
   },
   methods: {
-    xxx(){
-      console.log('xxx')
+    editTodo(e,item){
+      let input = this.$refs[item.id][0]
+      input.classList.add('editing')
+      input.focus()
+      this.editingTodo = item
+      this.beforeEditCache = item.content
+    },
+    comfirmEdit(item){
+      let input = this.$refs[item.id][0]
+      input.classList.remove('editing')
+      if(!this.editingTodo) return
+      this.editingTodo = null
+      if(!item.content.trim()){
+        this.removeTodo(item)
+      }
+    },
+    addTodo(e){
+      let value = this.todoContent && this.todoContent.trim()
+      if(!value) return
+      if(value){
+        this.todoList.unshift({
+          id: Math.round(Math.random()*100000),
+          content: value,
+          complete: false
+        })
+      }
+      e.currentTarget.blur()
+      this.todoContent = ''
+      todoStorage.save(this.todoList)
+    },
+    cancelEdit(e,item){
+      this.editingTodo = null
+      item.content = this.beforeEditCache
+      e.currentTarget.blur()
+    },
+    removeTodo(todo){
+      let index = this.todoList.findIndex( item => {
+        return item.id === todo.id
+      })
+      this.todoList.splice(index,1)
+    },
+    completeTodo(item){
+      item.complete = !item.complete
+    }
+  },
+  created(){
+    this.todoList = todoStorage.get()
+  },
+  watch:{
+    todoList: {
+      handler(todos){
+        todoStorage.save(todos)
+      },
+      deep: true
     }
   }
 }
@@ -49,72 +119,111 @@ export default {
 
 <style lang="scss">
 
-  $border-color: #ddd;
-  $checked-bg: #eee;
+  @mixin positionCenter {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  $edit-color: #4e90f2;
+  $delete-color: #ff7573;
+  $complete-color: #44e99a;
+
   #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  .wrapper{
-    width: 400px;
-    box-shadow: 0 0 4px rgba(0,0,0,0.2);
-    margin: 40px auto;
-    header {
-      height: 48px;
-      width: 400px;
-      line-height: 48px;
-      font-weight: 800;
-      font-size: 24px;
-      padding-left: 16px;
-      padding-top: 4px;
+  header {
+    @include positionCenter;
+    width: 100%;
+    margin-top: 2%;
+    display: flex;
+    flex-direction: column;
+    h1{
+      margin-bottom: 20px;
+      font-size: 40px;
+      color: #ddd;
     }
-    input{
-      &.todo{
-        height: 40px;
-        width: 400px;
-        border: none;
-        outline: none;
-        padding: 8px 16px 0 16px;
-        border-bottom: 1px solid $border-color;
-        &::placeholder {
-          color: #ccc;
-        }
+    .todo-input {
+      padding: 0 10px;
+      height: 56px;
+      width: 50%;
+      outline: none;
+      border: 1px solid #eee;
+      background: #fbfbfb;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: all 0.3s;
+      &::placeholder {
+        color: #ccc
+      }
+      &:focus {
+        background: #fff;
+        border: 1px solid #ddd;
+        box-shadow: 0 4px 8px -4px rgba(0,0,0,0.1),0 14px 16px -4px rgba(0,0,0,0.06);
       }
     }
-    section{
-      ul {
-        margin-top: -1px;
-        li {
-          height: 44px;
-          width: 100%;
-          line-height: 44px;
-          border-top: 1px solid $border-color;
-          padding: 0 16px;
-          .el-checkbox + label {
-            position: absolute;
-            padding: 0 10px;
-            width: 320px;
+  }
+  section {
+    @include positionCenter;
+    width: 100%;
+    ul{ width: 50%; margin-top: 16px;
+      li{
+        padding: 10px 0;
+        line-height: 20px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        position: relative;
+        border-bottom: 1px solid #f1f1f1;
+        &:hover{
+          .actions {
+            visibility: visible;
           }
-          input[type="text"] {
-            height: 42px;
-            border: none;
-            margin-top: -1px;
-            padding: 0 10px;
-            width: 320px ;
-            font-size: inherit;
-            position: relative;
-            display: none;
-            outline: none;
+        }
+        &.completed {
+          .item-content { color: #999; text-decoration: line-through;}
+        }
+        .item-content { color: #333;}
+        .edit-content {
+          display: none;
+          outline: none;
+          border: none;
+          height: 100%;
+          width: calc( 100% - 96px );
+          position: absolute;
+          font-size: 14px;
+          padding: 0 10px;
+          &.editing {
+            display: block;
           }
-          label[for="todo"]{
-            position: absolute;
-            padding-left: 10px;
+        }
+        .actions {
+          font-size: 16px;
+          display: flex;
+          visibility: hidden;
+          i{
+            @include positionCenter;
+            height: 32px;
+            width: 32px;
+            border-radius: 50%;
+            color: #999;
+            &:hover{
+              cursor: pointer;
+            }
+          }
+          .edit:hover {
+            color: $edit-color;
+          }
+          .check:hover {
+            color: $complete-color;
+          }
+          .delete:hover {
+            color: $delete-color;
           }
         }
       }
     }
   }
-
 }
 </style>
